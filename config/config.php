9f02b74b9301304968c45f334782885bc3fa6ff2
PaulHumeniuk <pavel.g@zlogic.io>
2022-03-18 10:58:02 +0200

    update

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/../.env');
 
 //site name
-define('SITE_NAME', $_ENV['SITE_NAME']);
+define('SITE_NAME', $_ENV['SITE_NAME'] ?? '');
 
 //App Root
 define('APP_ROOT', dirname(dirname(__FILE__)));
@@ -16,9 +16,9 @@ define('URL_ROOT', '/');
 define('URL_SUBFOLDER', '');
 
 //DB Params
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASS', $_ENV['DB_PASS']);
-define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_HOST', $_ENV['DB_HOST'] ?? '');
+define('DB_USER', $_ENV['DB_USER'] ?? '');
+define('DB_PASS', $_ENV['DB_PASS'] ?? '');
+define('DB_NAME', $_ENV['DB_NAME'] ?? '');
 
-define('IPSTACK_KEY', $_ENV['IPSTACK_KEY']);
+define('IPSTACK_KEY', $_ENV['IPSTACK_KEY'] ?? '');
